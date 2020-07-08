229ef7c897c85b815f685b65fc2e7bd3a7986d5d
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-07 23:01:40 -0400

    added notes route

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-
+app.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname + "/public/notes.html"));
+});
 // API / JSON
 
 
