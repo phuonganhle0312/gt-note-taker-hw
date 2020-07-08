4d05306f58b5f0f3f6110f5703c869252cdbbee5
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-07 21:44:45 -0400

    ran npm install to require express package and added view route

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const express = require("express");
 const app =  express ();
 //add a port 
 const PORT= process.env.PORT || 3000;
+
+// sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// View / HTML
+app.get("/", (req, res) =>{
+    res.send("Hello world;")
+})
+
+// API / JSON
+
+
 //listen on port
 app.listen(PORT, (req, res) => {
     console.log(`Currently running on http://localhost:${PORT}`);
-})
+});
